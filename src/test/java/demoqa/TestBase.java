609675bc930c466e6698605e709ea2d348ca8df1
svasenkov <aanher@gmail.com>
2022-12-09 21:44:49 +0300

    added remote url

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -13,6 +13,9 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
     }
 
     @BeforeEach
