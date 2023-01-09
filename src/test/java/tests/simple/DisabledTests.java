74416b860bdbdb2842a40bc8549124a5c5220efb
qasniffer0 <qasniffer0@gmail.com>
2023-01-09 21:25:27 +0500

    add notifications

--- src/test/java/tests/simple/DisabledTests.java
+++ src/test/java/tests/simple/DisabledTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@Tag("simple")
 @Tag("smoke")
 public class DisabledTests {
     @Test
--- src/test/java/tests/simple/PositiveTests.java
+++ src/test/java/tests/simple/PositiveTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Tag("simple")
 public class PositiveTests {
     @Test
     void test1() {
